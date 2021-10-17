fec35ee1ef12abde327037c6403f9175c38d3cfe
kotahorii <khorii.micron@gmail.com>
2021-10-17 22:04:48 +0900

    [update] prime timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Counter } from "./Counter";
 
 function App() {
   return (
     <div className="App">
-    <
+      <Counter limit={60} />
     </div>
   );
 }
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,5 +1,38 @@
-import { VFC } from "react";
+import { useEffect, useMemo, useState, VFC } from "react";
+import { clearInterval, setInterval } from "timers";
 
-export const Counter: VFC = () => {
-  return <div></div>;
+export const getPrimes = (maxRange: number): number[] =>
+  [...Array(maxRange + 1)]
+    .map((_, i) => i)
+    .slice(2)
+    .filter((n) => {
+      for (let i = 2; i < n; i += 1) {
+        if (n % i === 0) return false;
+      }
+      return true;
+    });
+
+console.log(getPrimes(10));
+
+export const Counter: VFC<{ limit: number }> = ({ limit }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const reset = (): void => setTimeLeft(limit);
+  const tick = (): void => setTimeLeft((t) => t - 1);
+  const primes = useMemo(() => getPrimes(limit), [limit]);
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timeLeft === 0) setTimeLeft(limit);
+  }, [timeLeft, limit]);
+  return (
+    <>
+      <p>{timeLeft}</p>
+      <button onClick={() => reset()}>Reset</button>
+    </>
+  );
 };
