553df710e07b05b9cb8859751e7154f97c5c9def
kotahorii <khorii.micron@gmail.com>
2021-10-19 13:56:07 +0900

    type union

--- src/type.ts
+++ src/type.ts
@@ -34,3 +34,15 @@ type TCurrency = {
   unit: Unit;
   amount: number;
 };
+
+type A = {
+  foo: number;
+  bar?: string;
+};
+type B = { foo: string };
+type C = { bar: string };
+type D = { baz: boolean };
+
+type AorB = A | B;
+type AorC = A | C;
+type AorD = A | D;
