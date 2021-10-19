6887398e57140b401b208a9fb9ac6ecaa1069a92
kotahorii <khorii.micron@gmail.com>
2021-10-19 13:20:10 +0900

    rest parameter

--- src/type.ts
+++ src/type.ts
@@ -3,3 +3,16 @@ export type Animal = "Dog" | "Cat" | "Rabbit";
 const arr1 = ["a", "b", "c"];
 const arr2 = [...arr1, "d", "e"];
 console.log(arr2);
+
+const obj1 = { a: 1, b: 2, c: 3, d: 4 };
+const obj2 = { ...obj1, d: 99, e: 5 };
+
+const user = {
+  id: 1,
+  name: "takshi",
+  email: "takashi@gmail.com",
+  age: 8,
+};
+
+const { id, ...rest } = user;
+console.log(id, rest);
